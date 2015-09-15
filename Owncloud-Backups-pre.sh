ea32c224957d9d06a7d70d4c543d0a0a7b2f034e
zanglerecke <zanglerecke.github@doiber.at>
2015-09-15 11:48:49 +0200

    Create Owncloud-Backups-pre.sh

---
+++ Owncloud-Backups-pre.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+#load config file
+if [ -s "/etc/default/Backup-Owncloud" ] ; then
+. /etc/default/Backup-Owncloud
+fi
+
+mysqldump --lock-tables -h $DBServer $DBName -u $DBUser --password=$DBPass > ${Target}
+#echo "Backup Successfull!"
+
